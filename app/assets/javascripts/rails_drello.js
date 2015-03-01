ef8152d2aaefadb83415f0ac0ecd40cf26ff62ff
DavideDaniel <davidedaniel@gmail.com>
2015-03-01 16:04:58 -0500

    updated routes

---
+++ app/assets/javascripts/rails_drello.js
@@ -0,0 +1,13 @@
+window.RailsDrello = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  RailsDrello.initialize();
+});

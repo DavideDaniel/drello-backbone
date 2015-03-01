e8c873d586aba9ee40eff47d1063d4af823d2b28
DavideDaniel <davidedaniel@gmail.com>
2015-03-01 06:59:36 -0500

    adding cards view

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,11 +1,18 @@
 class BoardController < ApplicationController
   def index
-  end
-
-  def new
+    def new
+      @provider = Provider.find_by(id: session[:provider_id])
+      binding.pry
+      if session[:provider_id]
+        @new_board = Board.new
+      else
+        redirect_to :back
+      end
+    end
   end
 
   def create
+    @board = Board.create(params_board)
   end
 
   def show
@@ -18,5 +25,10 @@ class BoardController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def params_board
+    params.require(:board).permit(:title, :provider_id, :consumer_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :boards
     resources :cards
   end
+  
+  resources :consumers
 
   resources :boards do
     resources :cards
